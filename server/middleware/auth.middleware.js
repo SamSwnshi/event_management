ccd169c267c34c1e06ee61971d37ffa879b5deb9
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-07-26 12:03:50 +0530

    middleware

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+
+export const auth = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+
+  if (!authHeader || !authHeader.startWith("Bearer")) {
+    return res
+      .status(401)
+      .json({ message: "Access Denied! No Token Provided" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    console.log("Decoded User: ", decoded);
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: "Invalid token" });
+  }
+};
+
+
+export const isAdmin = (req, res, next){
+    if(req.user.role === 'admin'){
+        next()
+    }else {
+        res.status(403).json({ message: 'Access denied' });
+    }
+}
