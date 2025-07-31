1a41ec1e22edb16c1926ecff77f99c7ea8ffc72f
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-07-31 22:21:43 +0530

    updated code

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://event-management-01g3.onrender.com/api",
 });
 
 api.interceptors.request.use(
